896e4862c5a4d7aa17eca0b44b9c436b99ec506d
Tetsu Yamashita <tetu_fs@live.jp>
2015-08-11 05:16:00 +0900

    おそらく現行使用中のcpp、ヘッダーファイルにすべてコメントを書き終わった。

--- Classes/stage.cpp
+++ Classes/stage.cpp
@@ -1,46 +1,65 @@
-﻿#include "Stage.h"
-
+﻿//Stage.hというヘッダーファイルを読む
+#include "Stage.h"
+//名前空間cocos2dを使うという宣言
 USING_NS_CC;
 
-
+//Stageクラスのコンストラクタ
+//_tiledMap(nullptr)「Stageクラスのpirvate変数_tiledMapに無を代入する」の意
 Stage::Stage()
 	: _tiledMap(nullptr)
 {
 }
-
+//Stageクラスのデストラクタ
 Stage::~Stage()
 {
 	CC_SAFE_RELEASE_NULL(_tiledMap);
 }
+//bool型のStage::init()関数を宣言
 
 bool Stage::init()
 {
+	//int型の変数x,yを宣言、初期化
 	int x = 0;
 	int y = 0;
 
+	//もしLayerが初期化されていなかったら返り値にfalseを返す
 	if (!Layer::init())
 	{
 		return false;
 	}
-
+	//TMXTiledMapのポインタ変数mapにTMXTiledMap::create("test_map.tmx")を代入
+	//test_map.tmxを読む
 	auto map = TMXTiledMap::create("test_map.tmx");
-	TMXLayer* background = map->getLayer("background"); // Tiledで設定したレイヤーの名前を渡す
-	TMXLayer* yuka = map->getLayer("yuka"); // Tiledで設定したレイヤーの名前を渡す
+	//TMXTiledMapのポインタ変数backgroundにmapのレイヤーbackgroundを代入
+	// Tiledで設定したレイヤーの名前を渡す
+	TMXLayer* background = map->getLayer("background");
+	//TMXTiledMapのポインタ変数yukaにmapのレイヤーyukaを代入
+	TMXLayer* yuka = map->getLayer("yuka");
+	
+	//なんとなくはわかるけどよくわからない
+	//ここでTiledで設定した値からマップチップを並べている？
 	background->getTileAt(Vec2(x, y));
 	background->getTileGIDAt(Vec2(x, y));
 	yuka->getTileAt(Vec2(x, y));
 	yuka->getTileGIDAt(Vec2(x, y));
+
+	//Stageクラスの表示場所を指定
 	this->setPosition(320, 240);
+	//Stageクラスのサイズを倍に指定
 	this->setScale(2.0f);
+	//Stageクラスの子にmapを加える（これで画像を描画だっけ）
 	this->addChild(map);
+	//StageクラスのTiledMapにmapを指定　おまじないかな
 	this->setTiledMap(map);
 
+	//Stageクラスを毎フレーム更新する
 	this->scheduleUpdate();
 
+	//返り値にtrueを返す
 	return true;
 
 }
-
+//毎フレーム更新する
 void Stage::update(float dt)
 {
 
--- Classes/stage.h
+++ Classes/stage.h
@@ -1,19 +1,37 @@
+//もしdefineされていなければ
 #ifndef __MyGame__stage__
+//コンパイル時に置き換える　インクルードガードというらしい。
+//ifndefとendifの間に挟むとエラーが出なくなるおまじない。
 #define __MyGame__stage__
-
+//cocos2d.hをよむ
 #include "cocos2d.h"
-
+//cocos2d::Layerクラスのパブリックで継承クラスStageを作る
 class Stage :public cocos2d::Layer
 {
+	//Stageクラス内でのみ
 protected:
+	//Stageコンストラクタの宣言
 	Stage();
+	//Stageデストラクタの宣言　virtualはおまじない
 	virtual ~Stage();
+
+	//bool型のinit()関数の宣言 overrideはおまじない
+	//CREATE_FUNC()に必須
 	bool init() override;
 
+	//クラス外からでも参照可能
 public:
+	//void型のupdate(float dt)関数の宣言　overrideはおまじない
 	void update(float dt) override;
+
 	//Tile
+	//TMXTiledMapという型をつくり、Stageクラスのprivate変数_tiledMapを作り、getTledMap/setTledMapをつくる
+	//ここでつくられたTMXTiledMap型の変数はポインタ変数になる
+	//getTledMapは_tiledMapを取得し、setTledMapは_tiledMapを書き換えられる
 	CC_SYNTHESIZE_RETAIN(cocos2d::TMXTiledMap *, _tiledMap, TiledMap);
+
+	//static Stage* create()と同じ意味。bool init();が必要
+	//Stageクラスインスタンスへのポインタを返すStageクラスのクラスメソッド、createを宣言している
 	CREATE_FUNC(Stage);
 };
 
